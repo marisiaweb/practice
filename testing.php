dadbda6c16bd58947c957b07d6857d5c31734a27
marisia <opensourcediva@gmail.com>
2016-10-08 22:25:26 +0000

    fix conflict

--- testing.php
+++ testing.php
@@ -1,7 +1,11 @@
 <?php
 
+<<<<<<< HEAD
 echo 'from branch 2';
 
 echo 'this is testing'; echo 'from branch 2'; 
 
 
+=======
+echo 'this is testing'; echo 'from branch 2';
+>>>>>>> parent of afd5ef6... testing on branch master
